061adb98b4e2a06e5fb7b878a00dd5f043325e58
cubicdaiya <cubicdaiya@gmail.com>
2014-01-11 16:19:32 +0900

    add test

--- gonp.go
+++ gonp.go
@@ -4,8 +4,8 @@
 package gonp
 
 import (
-	"math"
 	"container/list"
+	"math"
 )
 
 type SesType int
@@ -29,12 +29,16 @@ type Diff struct {
 	A    string
 	B    string
 	m, n int
-	reverse bool
-	path []int
-	pathposi map[int]Point
 	ed   int
+	ctl  *Ctl
 	lcs  *list.List
 	ses  *list.List
+}
+
+type Ctl struct {
+	reverse  bool
+	path     []int
+	pathposi map[int]Point
 }
 
 func max(x, y int) int {
@@ -43,18 +47,31 @@ func max(x, y int) int {
 
 func New(a string, b string) *Diff {
 	m, n := len(a), len(b)
+	diff := new(Diff)
+	ctl := new(Ctl)
 	if m > n {
-		return &Diff{A:b, B:a, m:n, n:m, reverse:true}
+		diff.A, diff.B = b, a
+		diff.m, diff.n = n, m
+		ctl.reverse = true
+	} else {
+		diff.A, diff.B = a, b
+		diff.m, diff.n = m, n
+		ctl.reverse = false
 	}
-	return &Diff{A:a, B:b, m:m, n:n, reverse:false}
+	diff.ctl = ctl
+	return diff
 }
 
 func (diff *Diff) Editdistance() int {
 	return diff.ed
 }
 
-func (diff *Diff) Lcs() *list.List {
-	return diff.lcs
+func (diff *Diff) Lcs() string {
+	var b = make([]byte, diff.lcs.Len())
+	for i, e := 0, diff.lcs.Front(); e != nil; i, e = i+1, e.Next() {
+		b[i] = e.Value.(byte)
+	}
+	return string(b)
 }
 
 func (diff *Diff) Ses() *list.List {
@@ -66,38 +83,28 @@ func (diff *Diff) Compose() {
 	delta := diff.n - diff.m
 	size := diff.m + diff.n + 3
 	fp := make([]int, size)
-	diff.path = make([]int, size)
-	diff.pathposi = make(map[int]Point)
+	diff.ctl.path = make([]int, size)
+	diff.ctl.pathposi = make(map[int]Point)
 	diff.lcs = list.New()
 	diff.ses = list.New()
+	ctl := diff.ctl
 
 	for i := range fp {
 		fp[i] = -1
-		diff.path[i] = -1
+		ctl.path[i] = -1
 	}
 
 	for p := 0; ; p++ {
 
-		done := make(chan bool)
-		go func() {
-			for k := -p; k <= delta-1; k++ {
-				fp[k+offset] = diff.snake(k, fp[k-1+offset]+1, fp[k+1+offset], offset)
-			}
-			done <- true
-		}()
-
-		go func() {
-			for k := delta + p; k >= delta+1; k-- {
-				fp[k+offset] = diff.snake(k, fp[k-1+offset]+1, fp[k+1+offset], offset)
-			}
-			done <- true
-		}()
-
-		for i := 0; i < 2; i++ {
-			<-done
+		for k := -p; k <= delta-1; k++ {
+			fp[k+offset] = diff.snake(k, fp[k-1+offset]+1, fp[k+1+offset], offset, diff.ctl)
 		}
 
-		fp[delta+offset] = diff.snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset], offset)
+		for k := delta + p; k >= delta+1; k-- {
+			fp[k+offset] = diff.snake(k, fp[k-1+offset]+1, fp[k+1+offset], offset, diff.ctl)
+		}
+
+		fp[delta+offset] = diff.snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset], offset, diff.ctl)
 
 		if fp[delta+offset] >= diff.n {
 			diff.ed = delta + 2*p
@@ -105,46 +112,47 @@ func (diff *Diff) Compose() {
 		}
 	}
 
-	r := diff.path[delta+offset]
+	r := ctl.path[delta+offset]
 	epc := make(map[int]Point)
 	for r != -1 {
-		epc[len(epc)] = Point{x:diff.pathposi[r].x, y:diff.pathposi[r].y, k:-1}
-		r = diff.pathposi[r].k
+		epc[len(epc)] = Point{x: ctl.pathposi[r].x, y: ctl.pathposi[r].y, k: -1}
+		r = ctl.pathposi[r].k
 	}
 	diff.recordSeq(epc)
 }
 
 func (diff *Diff) recordSeq(epc map[int]Point) {
-	x_idx,  y_idx  := 1, 1
+	x_idx, y_idx := 1, 1
 	px_idx, py_idx := 0, 0
-	for i:=len(epc)-1;i!=0;i-- {
+	ctl := diff.ctl
+	for i := len(epc) - 1; i != 0; i-- {
 		for (px_idx < epc[i].x) || (py_idx < epc[i].y) {
 			var t SesType
 			if (epc[i].y - epc[i].x) > (py_idx - px_idx) {
 				elem := diff.B[py_idx]
-				if diff.reverse {
+				if ctl.reverse {
 					t = Delete
 				} else {
 					t = Add
 				}
-				diff.ses.PushBack(SesElem{c:elem, t:t})
+				diff.ses.PushBack(SesElem{c: elem, t: t})
 				y_idx++
 				py_idx++
-			} else if epc[i].y - epc[i].x < py_idx - px_idx {
+			} else if epc[i].y-epc[i].x < py_idx-px_idx {
 				elem := diff.A[px_idx]
-				if diff.reverse {
+				if ctl.reverse {
 					t = Add
 				} else {
 					t = Delete
 				}
-				diff.ses.PushBack(SesElem{c:elem, t:t})
+				diff.ses.PushBack(SesElem{c: elem, t: t})
 				x_idx++
 				px_idx++
 			} else {
-				elem := diff.A[x_idx]
+				elem := diff.A[px_idx]
 				t = Common
 				diff.lcs.PushBack(elem)
-				diff.ses.PushBack(SesElem{c:elem, t:t})
+				diff.ses.PushBack(SesElem{c: elem, t: t})
 				x_idx++
 				y_idx++
 				px_idx++
@@ -154,12 +162,12 @@ func (diff *Diff) recordSeq(epc map[int]Point) {
 	}
 }
 
-func (diff *Diff) snake(k, p, pp, offset int) int {
+func (diff *Diff) snake(k, p, pp, offset int, ctl *Ctl) int {
 	r := 0
 	if p > pp {
-		r = diff.path[k-1+offset];
+		r = ctl.path[k-1+offset]
 	} else {
-		r = diff.path[k+1+offset];
+		r = ctl.path[k+1+offset]
 	}
 
 	y := max(p, pp)
@@ -170,8 +178,8 @@ func (diff *Diff) snake(k, p, pp, offset int) int {
 		y += 1
 	}
 
-	diff.path[k+offset] = len(diff.pathposi)
-	diff.pathposi[len(diff.pathposi)] = Point{x:x, y:y, k:r}
- 
+	ctl.path[k+offset] = len(ctl.pathposi)
+	ctl.pathposi[len(ctl.pathposi)] = Point{x: x, y: y, k: r}
+
 	return y
 }
--- gonp_test.go
+++ gonp_test.go
@@ -16,8 +16,26 @@ func init() {
 	runtime.GOMAXPROCS(cpus)
 }
 
-func TestEditdistance(t *testing.T) {
+func TestDiff1(t *testing.T) {
 	diff := New("abc", "abd")
 	diff.Compose()
+	lcs := diff.Lcs()
 	assert(t, diff.Editdistance() == 2)
+	assert(t, lcs == "ab")
+}
+
+func TestDiff2(t *testing.T) {
+	diff := New("abcdef", "dacfea")
+	diff.Compose()
+	lcs := diff.Lcs()
+	assert(t, diff.Editdistance() == 6)
+	assert(t, lcs == "acf")
+}
+
+func TestDiff3(t *testing.T) {
+	diff := New("acbdeacbed", "acebdabbabed")
+	diff.Compose()
+	lcs := diff.Lcs()
+	assert(t, diff.Editdistance() == 6)
+	assert(t, lcs == "acbdabed")
 }
