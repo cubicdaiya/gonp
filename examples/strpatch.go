626233e27c7294bf6b734ba3ae140fc4f8282643
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2022-04-10 16:47:02 +0900

    examples: added valid string check.

--- examples/strpatch.go
+++ examples/strpatch.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"unicode/utf8"
 
 	"github.com/cubicdaiya/gonp"
 )
@@ -11,6 +12,13 @@ import (
 func main() {
 	if len(os.Args) < 3 {
 		log.Fatal("./strdiff arg1 arg2")
+	}
+	if !utf8.ValidString(os.Args[1]) {
+		log.Fatalf("arg1 contains invalid rune")
+	}
+
+	if !utf8.ValidString(os.Args[2]) {
+		log.Fatalf("arg2 contains invalid rune")
 	}
 	a := []rune(os.Args[1])
 	b := []rune(os.Args[2])
