f43bf5c052c24850f0e166d1604c500e4e4b7c2e
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2022-04-25 12:28:06 +0900

    test: added benchmark for Patch() and UniPatch().

--- patch_test.go
+++ patch_test.go
@@ -1,9 +1,20 @@
 package gonp
 
 import (
+	"strings"
 	"testing"
 	"unicode/utf8"
 )
+
+func buildLongString(prefix, interfix, suffix string, n int) string {
+	var sb strings.Builder
+	sb.WriteString(prefix)
+	for i := 0; i < n; i++ {
+		sb.WriteString(interfix)
+	}
+	sb.WriteString(suffix)
+	return sb.String()
+}
 
 func TestPatch(t *testing.T) {
 
@@ -82,6 +93,52 @@ func TestPatch(t *testing.T) {
 		if string(uniPatchedSeq) != test.b {
 			t.Errorf("applying unified format difference between '%s' and '%s' to '%s' is %s, but got %s", string(test.a), string(test.b), string(test.a), string(test.b), string(uniPatchedSeq))
 		}
+	}
+}
+
+func BenchmarkPatchSmall(b *testing.B) {
+	s1 := []rune("abc")
+	s2 := []rune("abd")
+	diff := New(s1, s2)
+	diff.Compose()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = diff.Patch(s1)
+	}
+}
+
+func BenchmarkUniPatchSmall(b *testing.B) {
+	s1 := []rune("abc")
+	s2 := []rune("abd")
+	diff := New(s1, s2)
+	diff.Compose()
+	uniHunks := diff.UnifiedHunks()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = diff.UniPatch(s1, uniHunks)
+	}
+}
+
+func BenchmarkPatchLarge(b *testing.B) {
+	s1 := []rune(buildLongString("abc", "a", "def", 1000))
+	s2 := []rune(buildLongString("abd", "a", "ghi", 1000))
+	diff := New([]rune(s1), []rune(s2))
+	diff.Compose()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = diff.Patch(s1)
+	}
+}
+
+func BenchmarkUniPatchLarge(b *testing.B) {
+	s1 := []rune(buildLongString("abc", "a", "def", 1000))
+	s2 := []rune(buildLongString("abd", "a", "ghi", 1000))
+	diff := New(s1, s2)
+	diff.Compose()
+	uniHunks := diff.UnifiedHunks()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = diff.UniPatch(s1, uniHunks)
 	}
 }
 
